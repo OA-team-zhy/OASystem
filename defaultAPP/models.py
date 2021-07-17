cabfea83aabe4ab1e68008d7cb20938333acacc6
OrtonY <907034418@qq.com>
2021-07-17 14:21:58 +0800

    login

--- defaultAPP/models.py
+++ defaultAPP/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+
+##用户注册
+class User(models.Model):
+    gender = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    gender = models.CharField(max_length=10, choices=gender, default='m', verbose_name="性别")
+    birthday = models.DateField(verbose_name="生日")
+    email = models.EmailField(verbose_name="邮箱")
+    info = models.CharField(max_length=255, verbose_name="个人简介", help_text="一句话介绍自己，不要超过250字")
+
+    no = models.CharField(max_length=4, verbose_name="注册编号")
+    number = models.CharField(max_length=6, verbose_name="用户编号")
+    password = models.CharField(max_length=30, verbose_name="密码")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['no', 'number'], name='user_id'),
+        ]
+
+    def get_id(self):
+        return self.no + self.number
+
+    def __str__(self):
+        return "%s (%s)" % (self.no + self.number, self.name)
+
+##管理者注册
+class Admin(models.Model):
+    genders = [
+        ("m", "男"),
+        ("f", "女")
+    ]
+
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    gender = models.CharField(max_length=10, choices=genders, default='m', verbose_name="性别")
+    birthday = models.DateField(verbose_name="生日")
+    email = models.EmailField(verbose_name="邮箱")
+    info = models.CharField(max_length=255, verbose_name="个人简介", help_text="不要超过250字")
+
+    no = models.CharField(max_length=3, verbose_name="注册编号")
+    number = models.CharField(max_length=7, verbose_name="管理员编号")
+    password = models.CharField(max_length=30, verbose_name="密码")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['no', 'number'], name='admin_id'),
+        ]
+
+    def get_id(self):
+        return self.no + self.number
+
+    def __str__(self):
+        return "%s (%s)" % (self.no + self.number, self.name)
--- defaultAPP/views.py
+++ defaultAPP/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
+from constants import INVALID_KIND
+from defaultAPP.forms import UserLoginForm, AdminLoginForm
+from defaultAPP.cbvs import CreateUserView,CreateAdminView
 
 # Create your views here.
+def home(request):
+    return render(request, "defaultAPP/login_home.html")
+
+def login(request, *args, **kwargs):
+    if not kwargs or "kind" not in kwargs or kwargs["kind"] not in ["admin", "user"]:
+        return HttpResponse(INVALID_KIND)
+
+    kind = kwargs["kind"]
+    context = {'kind':kind}
+
+    if request.method == 'POST':
+        if kind == "admin":
+            form = AdminLoginForm(data=request.POST)
+        else:
+            form = UserLoginForm(data=request.POST)
+
+        if form.is_valid():
+            uid = form.cleaned_data["uid"]
+
+            temp_res = "hello, %s" % uid
+            return HttpResponse(temp_res)
+        else:
+            context['form'] = form
+    elif request.method == 'GET':
+        if request.GET.get('uid'):
+            uid = request.GET.get('uid')
+            context['uid'] = uid
+            data = {"uid":uid, 'password':'12345678'}
+            if kind == "admin":
+                form = AdminLoginForm(data)
+            else:
+                form = UserLoginForm
+        else:
+            if kind == "admin":
+                form = AdminLoginForm()
+            else:
+                form = UserLoginForm()
+
+        context['form'] = form
+        if request.GET.get('from_url'):
+            context['from_url'] = request.GET.get('from_url')
+
+        return render(request, 'defaultAPP/login_detail.html', context)
+
+def register(request, kind):
+    func = None
+    if kind == "user":
+        func = CreateUserView.as_view()
+    elif kind == "admin":
+        func = CreateAdminView.as_view()
+
+    if func:
+        return func(request)
+    else:
+        return HttpResponse(INVALID_KIND)
